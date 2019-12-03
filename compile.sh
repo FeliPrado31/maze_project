915c81681d3ea08c3786f8ee6f1f4f4632003c46
Cybernuki <1164@holbertonschool.com>
2019-12-02 20:44:31 -0500

    fix an error in the script

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 gcc -c `sdl2-config --cflags` *.c;
-mv *.o ./compiled_files;
-gcc ./compiled_files/*.o `sdl2-config --libs` -lSDL2_ttf -o exe;
+mv *.o ./c_files;
+gcc ./c_files/*.o `sdl2-config --libs` -lSDL2_ttf -o exe;
